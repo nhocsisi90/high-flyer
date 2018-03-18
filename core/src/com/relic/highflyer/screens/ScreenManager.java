d9efb21c485a4799eb00ab932eedcc7928ef1e4f
Sang Huynh <shuynh@changhealthcare.com>
2018-03-18 13:37:21 -0700

    Rename packages and app-name to high-flyer

--- core/src/com/relic/highflyer/screens/ScreenManager.java
+++ core/src/com/relic/highflyer/screens/ScreenManager.java
@@ -3,23 +3,21 @@ package com.relic.highflyer.screens;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.utils.Disposable;
-import com.relic.highflyer.MyGdxGame;
+import com.relic.highflyer.GameEngine;
 
 public class ScreenManager implements Disposable {
 
-	private static final String WORLD_MAP = "world_map";
 	private static final ScreenManager INSTANCE = new ScreenManager();
 
-	private MyGdxGame game;
+	private GameEngine game;
 	private Map<String, Screen> screens = new HashMap<>();
 
 	private ScreenManager() {
 	}
 
-	public static void init(MyGdxGame game) {
+	public static void init(GameEngine game) {
 		INSTANCE.game = game;
 	}
 
@@ -28,36 +26,9 @@ public class ScreenManager implements Disposable {
 	}
 
 	public Screen getInitial() {
-		Screen screen = new WorldScreen(game);
-		screens.put(WORLD_MAP, screen);
+		Screen screen = new Level1(game);
+		screens.put(Level1.class.getSimpleName(), screen);
 		return screen;
-	}
-
-	public Screen setCurrent(String location) {
-		Screen screen = null;
-		if (WORLD_MAP.equals(location)) {
-			screen = screens.get(location);
-			if (screen == null) {
-				screen = new WorldScreen(game);
-				screens.put(location, screen);
-			}
-		} else if ("lake_haven".equals(location)) {
-			screen = screens.get(location);
-			if (screen == null) {
-				screen = new CityScreen(game);
-				screens.put(location, screen);
-			}
-		}
-
-		if (screen == null) {
-			throw new IllegalArgumentException(String.format("Screen %s not found", location));
-		}
-
-		Gdx.app.log("Entering location", location.toString());
-
-		game.setScreen(screen);
-		return screen;
-
 	}
 
 	@Override
--- desktop/src/com/relic/highflyer/desktop/DesktopLauncher.java
+++ desktop/src/com/relic/highflyer/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.relic.highflyer.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.relic.highflyer.MyGdxGame;
+import com.relic.highflyer.GameEngine;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new GameEngine(), config);
 	}
 }
